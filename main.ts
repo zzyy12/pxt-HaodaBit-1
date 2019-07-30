70a4b0165a16a9a20e64eb643c5098f8321349ef
zzyy12 <43592960+zzyy12@users.noreply.github.com>
2019-07-30 14:35:46 +0800

    ir up

--- main.ts
+++ main.ts
@@ -256,7 +256,18 @@ namespace HaodaBit {
 	let dht11Temp = -1;
     let dht11Humi = -1;
 	let tcs34725Initialised = false
-	let initddd = false;
+	let tempHandler: Action;
+    let send_init = false;
+    let rec_init = false;
+    let arr: number[] = []
+    let received = false
+    let first = true
+    let rec_Type = ""
+    let messageStr = ""
+    let command1 = 0
+    let recPin = DigitalPin.P8
+    let thereIsHandler = false
+    arr = []
 	let irLed = AnalogPin.P16;
 	let pwmPeriod = 26;
     pins.analogWritePin(irLed, 0);
@@ -343,13 +354,121 @@ namespace HaodaBit {
         return Math.floor(ret * 10 / 6 / 58);
     }
 	
-	//% blockId=IR_read block="read IR %pin"
-    //% weight=100
+	
+	   /**
+     *  set the IR receiver pin.
+     */
+    //% blockId=setREC_pin block="set IR receiver pin: %myPin" 
+    //% weight=85 
 	//% group="传感器" blockGap=40
-    export function IR_read(pin: Ports): number {
-        haodabitInit(pin)
-        return getParam()
+    export function setREC_pin(myPin: DigitalPin) {
+        recPin = myPin;
+        pins.setEvents(recPin, PinEventType.Pulse)
+        pins.setPull(recPin, PinPullMode.PullUp)
+        pins.onPulsed(recPin, PulseValue.Low, function () {
+            arr.push(input.runningTimeMicros())
+        })
+        pins.onPulsed(recPin, PulseValue.High, function () {
+            arr.push(input.runningTimeMicros())
+        })
+        control.onEvent(recPin, DAL.MICROBIT_PIN_EVENT_ON_TOUCH, tempHandler);
+        rec_init = true;
     }
+	
+	 function resetReceiver() {
+        arr = []
+        received = false
+    }
+
+    control.inBackground(function () {
+        basic.forever(function () {
+            if ((!received) && (rec_init)) {
+                if (arr.length > 20) {
+                    if ((input.runningTimeMicros() - arr[arr.length - 1]) > 120000) {
+                        if (first) {
+                            resetReceiver()
+                            first = false
+                        } else {
+                            received = true
+                            decodeIR();
+                        }
+                    }
+                }
+            }
+        })
+    })
+
+    function decodeIR() {
+        let addr = 0
+        let command = 0
+        messageStr = ""
+        rec_Type = ""
+        for (let i = 0; i <= arr.length - 1 - 1; i++) {
+            arr[i] = arr[i + 1] - arr[i]
+        }
+        if (((arr[0] + arr[1]) > 13000) && ((arr[0] + arr[1]) < 14000)) {
+            rec_Type = "NEC"
+            arr.removeAt(1)
+            arr.removeAt(0)
+            addr = pulseToDigit(0, 15, 1600)
+            command = pulseToDigit(16, 31, 1600)
+            command1 = command & 0x00ff
+
+            arr = [];
+            if (thereIsHandler) {
+                tempHandler();
+            }
+        } else if (((arr[0] + arr[1]) > 2600) && ((arr[0] + arr[1]) < 3200)) {
+            rec_Type = "SONY"
+            arr.removeAt(1)
+            arr.removeAt(0)
+            command = pulseToDigit(0, 11, 1300)
+            command1 = command & 0x00ff
+            arr = [];
+            if (thereIsHandler) {
+                tempHandler();
+            }
+        }
+        resetReceiver();
+    }
+
+    function pulseToDigit(beginBit: number, endBit: number, duration: number): number {
+        let myNum = 0
+        for (let i = beginBit; i <= endBit; i++) {
+            myNum <<= 1
+            if ((arr[i * 2] + arr[i * 2 + 1]) < duration) {
+                myNum += 0
+            } else {
+                myNum += 1
+            }
+        }
+        return myNum
+    }
+
+
+    /**
+     * Do something when a receive IR
+     */
+    //% blockId=onReceivedIR block="on IR message received" blockInlineInputs=true
+    //% weight=70 
+	//% group="传感器" blockGap=40
+    export function onReceivedIR(handler: Action): void {
+        tempHandler = handler
+        thereIsHandler = true
+    }
+
+
+    /**
+     * return the message of the received IR 
+     */
+    //% blockId=getMessage block="read IR"
+    //% weight=60 
+	//% group="传感器" blockGap=40
+    export function getMessage(handler: Action): number {
+
+        return command1
+    }
+	
 
     //% blockId=IR_KEY block="IR buttons| %readkey"
     //% weight=100
@@ -720,8 +839,8 @@ namespace HaodaBit {
         irLed = portss;
         pins.analogWritePin(irLed, 0);
         pins.analogSetPeriod(irLed, pwmPeriod);
-        initddd = true;
-        if (initddd) {
+        send_init = true;
+        if (send_init) {
             //control.inBackground(() => {
                 sendMessage(msg, times, encodingType.NEC);
             //})
