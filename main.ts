ae023f8dd29f253b85936add0062e203f25d6a00
zzyy12 <43592960+zzyy12@users.noreply.github.com>
2019-08-13 10:20:21 +0800

    错误排查6

--- main.ts
+++ main.ts
@@ -747,95 +747,121 @@ namespace HaodaBit {
     }
 	
 	
-    function transmitBit(highTime: number, lowTime: number): void {
-        pins.analogWritePin(irLed, 512);
-        control.waitMicros(highTime);
-        pins.analogWritePin(irLed, 0);
-        control.waitMicros(lowTime);
-    }
+
 	
 	
-
-    function encode(myCode: number, bits: number, trueHigh: number, trueLow: number, falseHigh: number, falseLow: number): void {
-        const MESSAGE_BITS = bits;
-        for (let mask = 1 << (MESSAGE_BITS - 1); mask > 0; mask >>= 1) {
-            if (myCode & mask) {
-                transmitBit(trueHigh, trueLow);
-            } else {
-                transmitBit(falseHigh, falseLow);
-            }
-        }
+	function setreg(reg: number, dat: number): void {
+        let buf = pins.createBuffer(2);
+        buf[0] = reg;
+        buf[1] = dat;
+        pins.i2cWriteBuffer(BMP280_I2C_ADDR, buf);
     }
 
-    function sendNEC(message: number, times: number): void {
-        const enum NEC {
-            startHigh = 9000,
-            startLow = 4500,
-            stopHigh = 560,
-            stopLow = 0,
-            trueHigh = 560,
-            trueLow = 1690,
-            falseHigh = 560,
-            falseLow = 560,
-            interval = 110000
-        }
-        let address = message >> 16;
-        let command = message % 0x010000;
-        const MESSAGE_BITS = 16;
-        let startTime = 0;
-        let betweenTime = 0;
-        for (let sendCount = 0; sendCount < times; sendCount++) {
-            startTime = input.runningTimeMicros();
-            transmitBit(NEC.startHigh, NEC.startLow);
-            encode(address, 16, NEC.trueHigh, NEC.trueLow, NEC.falseHigh, NEC.falseLow);
-            encode(command, 16, NEC.trueHigh, NEC.trueLow, NEC.falseHigh, NEC.falseLow);
-            transmitBit(NEC.stopHigh, NEC.stopLow);
-            betweenTime = input.runningTimeMicros() - startTime
-            if (times > 0)
-                control.waitMicros(NEC.interval - betweenTime);
-        }
+    function getreg(reg: number): number {
+        pins.i2cWriteNumber(BMP280_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BMP280_I2C_ADDR, NumberFormat.UInt8BE);
     }
 
-
-
-    export function sendMessage(message: number, times: number, myType: encodingType): void {
-        switch (myType) {
-            case encodingType.NEC: sendNEC(message, times);
-            default: sendNEC(message, times);
-        }
+    function getUInt16LE(reg: number): number {
+        pins.i2cWriteNumber(BMP280_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BMP280_I2C_ADDR, NumberFormat.UInt16LE);
     }
-	    
+
+    function getInt16LE(reg: number): number {
+        pins.i2cWriteNumber(BMP280_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BMP280_I2C_ADDR, NumberFormat.Int16LE);
+    }
+
+    let dig_T1 = getUInt16LE(0x88)
+    let dig_T2 = getInt16LE(0x8A)
+    let dig_T3 = getInt16LE(0x8C)
+    let dig_P1 = getUInt16LE(0x8E)
+    let dig_P2 = getInt16LE(0x90)
+    let dig_P3 = getInt16LE(0x92)
+    let dig_P4 = getInt16LE(0x94)
+    let dig_P5 = getInt16LE(0x96)
+    let dig_P6 = getInt16LE(0x98)
+    let dig_P7 = getInt16LE(0x9A)
+    let dig_P8 = getInt16LE(0x9C)
+    let dig_P9 = getInt16LE(0x9E)
+    setreg(0xF4, 0x2F)
+    setreg(0xF5, 0x0C)
+    let T = 0
+    let P = 0
+
+    function get(): void {
+        let adc_T = (getreg(0xFA) << 12) + (getreg(0xFB) << 4) + (getreg(0xFC) >> 4)
+        let var1 = (((adc_T >> 3) - (dig_T1 << 1)) * dig_T2) >> 11
+        let var2 = (((((adc_T >> 4) - dig_T1) * ((adc_T >> 4) - dig_T1)) >> 12) * dig_T3) >> 14
+        let t = var1 + var2
+        T = Math.idiv(((t * 5 + 128) >> 8), 100)
+        var1 = (t >> 1) - 64000
+        var2 = (((var1 >> 2) * (var1 >> 2)) >> 11) * dig_P6
+        var2 = var2 + ((var1 * dig_P5) << 1)
+        var2 = (var2 >> 2) + (dig_P4 << 16)
+        var1 = (((dig_P3 * ((var1 >> 2) * (var1 >> 2)) >> 13) >> 3) + (((dig_P2) * var1) >> 1)) >> 18
+        var1 = ((32768 + var1) * dig_P1) >> 15
+        if (var1 == 0)
+            return; // avoid exception caused by division by zero
+        let adc_P = (getreg(0xF7) << 12) + (getreg(0xF8) << 4) + (getreg(0xF9) >> 4)
+        let _p = ((1048576 - adc_P) - (var2 >> 12)) * 3125
+        _p = Math.idiv(_p, var1) * 2;
+        var1 = (dig_P9 * (((_p >> 3) * (_p >> 3)) >> 13)) >> 12
+        var2 = (((_p >> 2)) * dig_P8) >> 13
+        P = _p + ((var1 + var2 + dig_P7) >> 4)
+    }
+
     /**
-     *  set the infrared LED pin.
+     * get pressure
      */
-    //% blockId=setIR_pin block="set IR LED pin: %port" 
-    //% weight=90 
-	//% group="红外" blockGap=8
+    //% blockId="BMP280_GET_PRESSURE" block="BMP280 get pressure"
+    //% weight=80
+	//% group="传感器" blockGap=8
+    export function pressure(): number {
+        get();
+        return P;
+    }
 
-    export function setIR_pin(port: Ports) {
-		let portss = PortAnalog[port]
-        irLed = portss;
-        pins.analogWritePin(irLed, 0);
-        pins.analogSetPeriod(irLed, pwmPeriod);
-        send_init = true;
-    }
-	
-	 /**
-     * send message from IR LED. You must set the message encoding type, send how many times, and the message.
+    /**
+     * get temperature
+    */
+    //% blockId="BMP280_GET_TEMPERATURE" block="BMP280 get temperature"
+    //% weight=80
+	//% group="传感器" blockGap=8
+    export function temperature(): number {
+        get();
+        return T;
+    } 
+
+    /**
+     * power on
+    */
+    //% blockId="BMP280_POWER_ON" block="BMP280 power On"
+    //% weight=80 
+	//% group="传感器" blockGap=8
+    export function PowerOn() {
+        setreg(0xF4, 0x2F)
+    } 
+
+    /**
+     * power off
      */
-    //% blockId=HaodaBit_sendMyMessage block="IR send message at: %msg"
-    //% weight=100
-	//% group="红外" blockGap=8
-  export function sendMyMessage(msg: number): void {
-        if (send_init) {
-            //control.inBackground(() => {
-                sendMessage(msg, 1, encodingType.NEC);
-            //})
-        }
+    //% blockId="BMP280_POWER_OFF" block="BMP280 power Off"
+    //% weight=80 
+	//% group="传感器" blockGap=8
+    export function PowerOff() {
+        setreg(0xF4, 0)
     }
-	
-	
-	
+
+    /**
+     * set I2C address
+     */
+    //% blockId="BMP280_SET_ADDRESS" block="BMP280 set address %addr"
+    //% weight=80
+	//% group="传感器" blockGap=8
+    export function Address(addr: BMP280_I2C_ADDRESS) {
+        BMP280_I2C_ADDR = addr
+    }
 	
 	
 	function MM32DDDD(): void {
